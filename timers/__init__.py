72a6d1999c36f709a6677673def925c6b45feb3a
xeiNg0de <alekseytilte@gmail.com>
2025-12-31 17:11:52 +0300

    delete alarm from init

--- timers/__init__.py
+++ timers/__init__.py
@@ -1,10 +1,8 @@
 from ._timers import every_day_at
-from .timer_alarm import timer_alarm
 from .timer_stat_day import timer_stat_day
 
 
 __all__ = (
     "every_day_at",
-    "timer_alarm",
     "timer_stat_day",
 )
